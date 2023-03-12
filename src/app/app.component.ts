80db63a72b19fec1186adeb9a26106672f1cb2ae
Petro.Zhuk Symphony <p.zhuk@gis-art.com>
2023-03-12 19:26:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+// import { CustomFilterOperations } from 'devextreme/data/custom_store';
+import { ViewChild, OnInit } from '@angular/core';
+import { DxDataGridComponent } from 'devextreme-angular';
+import { trips } from "../trips";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app-name';
+export class AppComponent implements OnInit {
+
+  @ViewChild(DxDataGridComponent, { static: false }) dataGrid: DxDataGridComponent;
+
+  dataSource: any[] = trips;
+  @HostBinding('class') get getClass() {
+    return Object.keys(this.screen.sizes).filter(cl => this.screen.sizes[cl]).join(' ');
+  }
+
+  constructor(private authService: AuthService,
+              private screen: ScreenService,
+              public appInfo: AppInfoService) { }
+
+  ngOnInit() {
+    this.dataGrid.instance.showColumnChooser();
+  }
+
+
+
+  onRowClick(e: any) {
+    let keys = e.component.getSelectedRowKeys();
+    let index = keys.indexOf(e.key);
+
+    if (index > -1) {
+      keys.splice(index, 1);
+    } else {
+      keys.push(e.key);
+    }
+
+    e.component.selectRows(keys);
+  }
+
 }
